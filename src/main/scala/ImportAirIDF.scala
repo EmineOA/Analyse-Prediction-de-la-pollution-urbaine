b025aae5ed3cd94cb8ac9c950cfd40cfb08f4e52
EmineOA <emine.ouldagatt@gmail.com>
2025-11-20 19:43:39 +0100

    Add files via upload

--- src/main/scala/ImportAirIDF.scala
+++ src/main/scala/ImportAirIDF.scala
@@ -17,11 +17,37 @@ object ImportAirIDF {
     //pour diminuer le bruit dans la console
     spark.sparkContext.setLogLevel("WARN")
 
+    //Précision sur les types
+    import org.apache.spark.sql.types._
+
+    val schema = StructType(Seq(
+      StructField("Identifiant station", StringType, true),
+      StructField("Nom de la Station", StringType, true),
+      StructField("Nom de la ligne", StringType, true),
+      StructField("Niveau de pollution aux particules", StringType, true),
+      StructField("Niveau de pollution", StringType, true),
+      StructField("Incertitude", StringType, true),
+      StructField("Recommandation de surveillance", StringType, true),
+      StructField("Action(s) QAI en cours", StringType, true),
+      StructField("Lien vers les mesures en direct", StringType, true),
+      StructField("Durée des mesures", StringType, true),
+      StructField("Mesures d’amélioration mises en place ou prévues", StringType, true),
+      StructField("stop_lon", DoubleType, true),
+      StructField("stop_lat", DoubleType, true),
+      StructField("point_geo", StringType, true),
+      StructField("pollution_air", StringType, true),
+      StructField("air", StringType, true),
+      StructField("niveau", StringType, true),
+      StructField("actions", StringType, true),
+      StructField("niveau_pollution", StringType, true)
+    ))
+
     //Lecture du CSV
     val df = spark.read
       .format("csv")
       .option("sep", ";")
       .option("header", true)
+      .schema(schema)
       .load("./data/idf_data.csv")
     println("=== Structure du dataset QUALITE AIR IDF ===")
 
